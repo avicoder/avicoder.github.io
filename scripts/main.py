f2160ff419c03446c80a4e39db9d2c0db6776439
avicoder <avicoder@outlook.com>
2021-09-06 12:34:57 +0800

    removed test data

--- scripts/main.py
+++ scripts/main.py
@@ -26,18 +26,3 @@ with open(API_JSON, 'w') as outfile:
     json.dump(output, outfile, indent=4)
 
 
-file1 = open('myfile.txt', 'w')
-L = ["This is Delhi \n", "This is Paris \n", "This is London \n"]
-s = "Hello\n"
-  
-# Writing a string to file
-file1.write(s)
-  
-# Writing multiple strings
-# at a time
-file1.writelines(L)
-  
-# Closing file
-file1.close()
-  
-
